068688c407842dee22ab15d9a23ac5f413e487e6
bryzZz <tebloev.nikita@mail.ru>
2022-10-01 20:26:19 +0800

    fix bug with calendar arrows when sidebar is not expanded

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -19,7 +19,7 @@ export const Calendar: React.FC<CalendarProps> = observer(() => {
                 </div>
                 <button className='Calendar__button' onClick={calendar.decrementMonth}>
                     <svg viewBox='0 0 96 96' xmlns='http://www.w3.org/2000/svg'>
-                        <path d='M69.8437,43.3876,33.8422,13.3863a6.0035,6.0035,0,0,0-7.6878,9.223l30.47,25.39-30.47,25.39a6.0035,6.0035,0,0,0,7.6878,9.2231L69.8437,52.6106a6.0091,6.0091,0,0,0,0-9.223Z' />
+                        <path d='M39.3756,48.0022l30.47-25.39a6.0035,6.0035,0,0,0-7.6878-9.223L26.1563,43.3906a6.0092,6.0092,0,0,0,0,9.2231L62.1578,82.615a6.0035,6.0035,0,0,0,7.6878-9.2231Z' />
                     </svg>
                 </button>
                 <button className='Calendar__button' onClick={calendar.incrementMonth}>
